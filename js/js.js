b20eba90f05549390bfb489d67c2e4270cdcb0e4
Nadiyah Ford <nadiyah.ford@gmail.com>
2014-11-24 15:55:50 -0800

    created buttons

---
+++ js/js.js
@@ -0,0 +1,45 @@
+// Grabs the User Weight from input box
+
+	var userWeight = document.getElementById("user_weight");
+		console.log(userWeight.value);
+
+// Submits the information
+
+// Initializes the buttons
+
+	var britishButton = document.getElementById("british_system");
+	var metricButton = document.getElementById("metric_system");
+
+// Functions that set a value when the user selects a system
+
+// 	var measurementSystem = null;
+
+// 	britishButton.addEventListener("click", function() {
+// 		measurementSystem = "british_system";
+// 		console.log("British system");
+// 	}
+
+// 	metricButton.addEventListener("click", function() {
+// 		measurementSystem = "metric_system";
+// 		console.log("Metric system");
+// 	}
+
+// // Sets the Earthly weight; base is pounds (British)
+
+// 	var earthWeight = null;
+
+// 	if (measurementSystem = "british_system")
+// 		earthWeight = userWeight;
+// 		console.log(userWeight);
+// 	else (measurementSystem = "metric_system")
+// 		earthWeight = userWeight * .454;
+// 		console.log(userWeight);	 
+
+// // Converting the weights into space weights
+
+// 	var sunWeight = userWeight * 27;
+
+// 	var moonWeight = userWeight * .2;	
+
+// // Displaying the resulting values 
+
